9260af4d2dc9faa03df14e8282974b51453cdc7f
Ben Stein <bumstain@gmail.com>
2016-06-15 18:07:59 +0100

    Added Google map to page using ngmap

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,4 +11,15 @@ angular.module("brimApp")
       console.log('markers', map.markers);
       console.log('shapes', map.shapes);
     });
+    var vm = this;
+      vm.message = 'You can not hide. :)';
+      NgMap.getMap().then(function(map) {
+      vm.map = map;
+    });
+    vm.callbackFunc = function(param) {
+      console.log('I know where you are ' + param +'.' + vm.message);
+      console.log('You are at' + vm.map.getCenter());
+    };
   });
+
+
