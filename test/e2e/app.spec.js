a9fe4acaf78d58ec5e13f473a6ebe43533eec4ee
rahulrama <rahulrama@gmail.com>
2016-06-15 15:29:35 +0100

    Feature test added

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,6 +1,13 @@
 describe("brimApp", function() {
   it("should get home page title", function() {
-    browser.get('/');
-    expect(browser.getTitle()).toEqual("Brim Photo Search");
+    browser.get('/')
+    expect(browser.getTitle()).toEqual("Brim Photo Search")
   });
+
+  it('should allow you to search for hashtags')
+  browser.get('/')
+  $('#searchTags').sendKeys('cat')
+  $('#searchTagsubmit').click()
+  expect($$('#returnedTags').first().getText()).toMatch('cat')
+  expect($$('#returnedTags').last().getText()).toMatch('cat')
 });
