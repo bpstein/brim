7dc1e68db55dd342a7e071d60b1baf92d505768c
rahulrama <rahulrama@gmail.com>
2016-06-15 17:49:05 +0100

    Error handler updated

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,22 +3,23 @@
 angular.module("brimApp")
   .controller("brimAppController", ['$scope', 'GetTagsFactory', function($scope, GetTagsFactory) {
     var self = this;
-
-    // var gettagSuccess = function(scope, res) {
-    //   if (res.meta.code !== 200) {
-    //     scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
-    //     return;
-    //   }
-    //   if (res.data.length > 0) {
-    //     self.tags = res.data;
-    //   } else {
-    //     scope.error = "This hashtag has returned no results";
-    //   }
-    // };
+    $scope.test="testing scope"
+    var getResponseSuccess = function(scope, res, err) {
+      if (res.meta.code !== 200) {
+        scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
+        return;
+      }
+      if (res.data.length > 0) {
+        return;
+      } else {
+        scope.error = err;
+      }
+    };
 
     self.getTags = function(tagsearch) {
 		GetTagsFactory.get(tagsearch).success(function(response) {
-			self.tags = response.data;
+			getResponseSuccess($scope, response, "This hashtag has returned no results" )
+      self.tags = response.data;
 		});
   }
 }]);
