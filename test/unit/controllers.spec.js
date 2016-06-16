630368685cbe41c93ece81660344335c007dc0c9
rahulrama <rahulrama@gmail.com>
2016-06-16 12:16:16 +0100

    Erro handler tests added

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -6,31 +6,41 @@ describe("brimAppController", function() {
   var httpBackend, scope;
   var data = [{name: 'cat'}, {name: 'catamaran'}, {name: 'catnip'}];
   var response1 = {
-    meta: 200,
+    meta: {code: 400, error_message: "Bad Request"},
     data: ''
   }
   var response2 = {
-    meta: 300,
+    meta: {code: 200},
     data: ''
   }
+
 
 
   beforeEach(inject(function($rootScope, $controller,GetTagsService,$httpBackend){
     scope = $rootScope.$new();
     service = GetTagsService;
-
     ctrl = $controller('brimAppController', {
         $scope: scope,
         GetTagsService: service
     });
-    spyOn(ctrl,'getResponseSuccess').and.returnValue();
     httpBackend = $httpBackend;
     httpBackend.expect('JSONP','https://api.instagram.com/v1/tags/search?q=cat&access_token=3414423759.9460433.24ba738c23824cbd82e82201dc10dc57&callback=JSON_CALLBACK').respond(data);
   }));
 
   it('gets all tags associated with a given tag', function() {
+    spyOn(ctrl,'getResponseSuccess').and.returnValue();
     ctrl.getTags('cat');
     expect(ctrl.tags).toEqual(data.data);
     httpBackend.flush();
   });
+
+  it("returns an error if the server didn't respond with a success", function() {
+    ctrl.getResponseSuccess(scope, response1, "This hashtag has returned no results");
+    expect(scope.error).toEqual("Bad Request");
+  })
+
+  it("returns an error if the search tag returns no results from the server", function() {
+    ctrl.getResponseSuccess(scope, response2, "This hashtag has returned no results");
+    expect(scope.error).toEqual("This hashtag has returned no results");
+  })
 });
