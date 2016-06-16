7ee8b241ccc808ab5231cd46b7bad3f11463bf2c
Ben Stein <bumstain@gmail.com>
2016-06-16 22:49:00 +0100

    Multiple markers display with different custom images

--- app/js/controllers/MapController.js
+++ app/js/controllers/MapController.js
@@ -1,11 +1,9 @@
 angular.module("brimApp")
 .controller('MapCtrl', function($scope, $element, locationFactory) {
   const mapEl = $element.find('gmap')[0];
-  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
   const mapOptions = {
     zoom: 13,
     center: {lat: 51.5020,lng: -0.1281}, 
-    icon: iconBase + 'http://www.eastcottvets.co.uk/uploads/Animals/gingerkitten.jpg'
   };
   const gmap = new google.maps.Map(mapEl, mapOptions);
   $scope.gmap = gmap;
--- app/js/services/LocationFactory.js
+++ app/js/services/LocationFactory.js
@@ -4,17 +4,20 @@ angular.module("brimApp")
     name: 'London Eye',
     color: 'darkblue',
     lat: 51.5033,
-    lng: -0.1195
+    lng: -0.1195,
+    url: 'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg'
   },{
     name: 'Westminster',
     color: 'green',
     lat: 51.4993,
-    lng: -0.1273
+    lng: -0.1273,
+    url: 'http://i.imgur.com/Q3cUg29.gif?fb'
   },{
     name: 'Trafalgar Square',
     color: 'fuchsia',
     lat: 51.5080,
-    lng: -0.1281
+    lng: -0.1281, 
+    url: 'http://46mbk22zqw0136sbkcey5gbxrv.wpengine.netdna-cdn.com/wp-content/uploads/2014/05/beach-umbrella.jpg'
   }];
   
   return photoLocations;
