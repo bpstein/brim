5691c40476169ec4494c084f3a28849a68a2ff7b
Ben Stein <bumstain@gmail.com>
2016-06-17 00:23:11 +0100

    Added polaroid styling to images

--- app/js/services/LocationFactory.js
+++ app/js/services/LocationFactory.js
@@ -2,22 +2,28 @@ angular.module("brimApp")
 .factory('locationFactory', function() {
   const photoLocations = [{
     name: 'London Eye',
-    color: 'darkblue',
+    caption: 'so many tourists',
     lat: 51.5033,
     lng: -0.1195,
     url: 'http://i.telegraph.co.uk/multimedia/archive/02830/cat_2830677b.jpg'
-  },{
+  }, {
     name: 'Westminster',
-    color: 'green',
+    caption: 'Blah blah blah',
     lat: 51.4993,
     lng: -0.1273,
-    url: 'http://i.imgur.com/Q3cUg29.gif?fb'
-  },{
+    url: 'https://i.ytimg.com/vi/-pzckbNyqfc/hqdefault.jpg'
+  }, {
     name: 'Trafalgar Square',
-    color: 'fuchsia',
+    caption: 'so many pigeons',
     lat: 51.5080,
     lng: -0.1281, 
     url: 'http://46mbk22zqw0136sbkcey5gbxrv.wpengine.netdna-cdn.com/wp-content/uploads/2014/05/beach-umbrella.jpg'
+  }, {
+    name: 'Kensington Palace',
+    caption: 'so much money',
+    lat: 51.5058,
+    lng: -0.1877, 
+    url: 'http://media4.s-nbcnews.com/i/newscms/2015_46/855171/prince-george-today-tease-151111_a2e1b23a653cccde6d5fa161550e4151.jpg'
   }];
   
   return photoLocations;
