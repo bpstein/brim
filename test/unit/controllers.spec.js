c1e8706987ab6ebf071cb2911681d6a7616d0e39
Ben Stein <bumstain@gmail.com>
2016-06-17 13:33:24 +0100

    Corrected protractor test syntax

---
+++ test/unit/controllers.spec.js
@@ -0,0 +1,10 @@
+describe("brimAppController", function() {
+  var controller;
+
+  beforeEach(module("brimApp"));
+
+  beforeEach(inject(function($controller) {
+    controller = $controller("brimAppController");
+  }));
+
+});
