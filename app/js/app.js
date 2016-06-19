d272b394b13b5004d9b2c47e6da234290180ccae
DXTimer <ibbakalov@gmail.com>
2016-06-19 14:43:28 +0100

    Added app routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,32 @@
-var brimApp = angular.module("brimApp", ["ngMap"])
+var brimApp = angular.module("brimApp", ["ngMap", "ngRoute", "ngMessages"])
+
+.config(function($routeProvider, $stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/home");
+
+  $stateProvider
+    .state('home' {
+      url: "/",
+      templateUrl: "../partials/home.html",
+      controller: "HomeController"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "../partials/login.html",
+      controller: 'AuthController as auth'
+    })
+    .state('dashboard', {
+      url: "/dashboard",
+      templateUrl: "../partials/dashboard.html",
+      controller: 'brimAppController as ctrl'
+    });
+})
 
 .directive('mapMarker', function () {
   return {
     restrict: 'E',
     controller: 'MapMarkerCtrl'
   };
-})
+});
 
 
   
