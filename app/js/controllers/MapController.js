f4fbcb780f5d6bff5368d976fa04b24f795afd6b
Ben Stein <bumstain@gmail.com>
2016-06-22 16:34:17 +0700

    Styling for map and search buttons

--- app/js/controllers/MapController.js
+++ app/js/controllers/MapController.js
@@ -8,6 +8,52 @@ angular.module("brimApp")
   };
 
   var gmap = new google.maps.Map(mapEl, mapOptions);
+
+  // var mapStyles = [
+  
+  // ];
+
+  gmap.set('styles', [
+  {
+      "featureType": "administrative",
+      "stylers": [
+        { "visibility": "on" }
+      ]
+    },{
+      "featureType": "poi",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },{
+      "featureType": "transit",
+      "stylers": [
+        { "visibility": "on" }
+      ]
+    },{
+      "featureType": "road",
+      "stylers": [
+        { "color": "#333", 
+          "visibility": "on"
+        }
+      ]
+    },{
+      "featureType": "landscape",
+      "stylers": [
+        { "color": "#e3f2f5", 
+          "visibility": "on"
+       }
+      ]
+    },{
+      "featureType": "water",
+      "stylers": [
+        { "visibility": "on" },
+        { "color": "#f3feff" }
+      ]
+    }
+]);
+
+
+
   $scope.gmap = gmap;
   $scope.locations = locationFactory;
 })
